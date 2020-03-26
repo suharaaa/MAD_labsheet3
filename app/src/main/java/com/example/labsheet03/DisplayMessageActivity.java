73d52d4b728a33f771357b4cdbd89cc8d9f971c5
suharaaa <suha.lyoko@hotmail.com>
2020-03-27 00:25:52 +0530

    lab03 changes

---
+++ app/src/main/java/com/example/labsheet03/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.labsheet03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
